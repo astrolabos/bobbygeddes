65006e8cd77a2e8ed69f94bb21f1de5bf733f18b
chapperz <benjaminstevenchaplin@gmail.com>
2014-02-11 21:07:28 +0000

    more realistic id being parsed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,19 +4,22 @@ angular.module('bobbygeddesApp')
   .controller('MainCtrl', function ($scope) {
     $scope.events = [
       {
-      title: 'Reading and Leeds Festival',
-      locations: ['Reading', 'Leeds'],
-      dates: ['19/07/2013', '20/07/2013', '21/07/2013']
+        id: 1,
+        title: 'Reading and Leeds Festival',
+        locations: ['Reading', 'Leeds'],
+        dates: ['19/07/2013', '20/07/2013', '21/07/2013']
       },
       {
-      title: 'Glasto',
-      locations: ['Glasto'],
-      dates: ['19/06/2013', '20/06/2013', '21/06/2013']
+        id: 2,
+        title: 'Glasto',
+        locations: ['Glasto'],
+        dates: ['19/06/2013', '20/06/2013', '21/06/2013']
       }
     ];
   })
   .controller('EventCtrl', function ($scope) {
     $scope.event = {
+      id: 1,
       title: 'Reading and Leeds Festival',
       locations: ['Reading', 'Leeds'],
       dates: ['19/07/2013', '20/07/2013', '21/07/2013']
