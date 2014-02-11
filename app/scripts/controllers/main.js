d8344054b4f8e14fb5aa7f3403435c6a1712fa09
chapperz <benjaminstevenchaplin@gmail.com>
2014-02-11 20:46:30 +0000

    add event list page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,23 @@
 
 angular.module('bobbygeddesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.events = {
-        title: 'Reading and Leeds Festival',
-        locations: ['Reading', 'Leeds'],
-        dates: ['19/07/2013', '20/07/2013', '21/07/2013']
-    };
+    $scope.events = [
+      {
+      title: 'Reading and Leeds Festival',
+      locations: ['Reading', 'Leeds'],
+      dates: ['19/07/2013', '20/07/2013', '21/07/2013']
+      },
+      {
+      title: 'Glasto',
+      locations: ['Glasto'],
+      dates: ['19/06/2013', '20/06/2013', '21/06/2013']
+      }
+    ];
   })
-    .controller('EventCtrl', function ($scope) {
-        $scope.events = {
-            title: 'Reading and Leeds Festival',
-            locations: ['Reading', 'Leeds'],
-            dates: ['19/07/2013', '20/07/2013', '21/07/2013']
-        };
-    });
+  .controller('EventCtrl', function ($scope) {
+    $scope.event = {
+      title: 'Reading and Leeds Festival',
+      locations: ['Reading', 'Leeds'],
+      dates: ['19/07/2013', '20/07/2013', '21/07/2013']
+      };
+  });
