926ad30797c6ce1ea8ce9d26191364cd01b6c693
chapperz <benjaminstevenchaplin@gmail.com>
2014-02-11 18:49:49 +0000

    Yo Angular commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bobbygeddesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
