855266bc52055d7bf69b3d5ba8b76c293e0f283b
chapperz <benjaminstevenchaplin@gmail.com>
2014-02-11 20:28:20 +0000

    change to a cleaner model

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,9 @@
 
 angular.module('bobbygeddesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.title = 'Reading and Leeds Festival';
-    $scope.locations = ['Reading', 'Leeds'];
-    $scope.dates = ['19/07/2013', '20/07/2013', '21/07/2013'];
+    $scope.events = {
+        title: 'Reading and Leeds Festival',
+        locations: ['Reading', 'Leeds'],
+        dates: ['19/07/2013', '20/07/2013', '21/07/2013']
+    };
   });
